efdf5813f28596dcfa23b994c477b876516d3cc1
laduniestu <duniedun@gmail.com>
2025-08-29 03:45:17 +0700

    add darkmode and toploader

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ThemeProvider } from "./theme-provider";
+import NextTopLoader from "nextjs-toploader";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <NextTopLoader />
+      {children}
+    </ThemeProvider>
+  );
+}
